e6a52b845dccf5f31efb615b6fa5153efd0a3ffe
Tristan Andrus <tristan.andrus@aggiemail.usu.edu>
2020-07-18 14:48:11 -0600

    Complete Bare-Bones tutorial
    Signed-off-by: Tristan Andrus <tristan.andrus@aggiemail.usu.edu>

--- Makefile
+++ Makefile
@@ -1,5 +1,19 @@
 SPECSDIR=specification
+BUILDDIR=build
 SPECIFICATION_SOURCES = $(wildcard $(SPECSDIR)/*.tex)
+AS="/opt/cross/bin/i686-elf-as"
+CC="/opt/cross/bin/i686-elf-gcc"
+
+all: kernel
+
+.PHONY: dirs
+dirs:
+	mkdir -p $(BUILDDIR)
+
+kernel: boot.s vga_text_kernel.c dirs 
+	$(AS) boot.s -o $(BUILDDIR)/boot.o
+	$(CC) -c vga_text_kernel.c -o $(BUILDDIR)/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+	$(CC) -T linker.ld -o $(BUILDDIR)/andros.bin -ffreestanding -O2 -nostdlib $(BUILDDIR)/boot.o $(BUILDDIR)/kernel.o
 
 .PHONY: spec
 #spec: SPECIFICATION_SOURCES
@@ -15,3 +29,14 @@ clean:
 	rm $(wildcard *.aux)
 	rm SpecTemplate.log texput.log
 
+.PHONY: iso
+iso: $(BUILDDIR)/AndrOS.iso
+
+.ONESHELL:
+$(BUILDDIR)/AndrOS.iso: kernel
+	cd $(BUILDDIR)
+	mkdir -p iso/boot/grub
+	cp andros.bin iso/boot
+	cp ../grub.cfg iso/boot/grub
+	pwd
+	grub-mkrescue -o AndrOS.iso iso
---
+++ boot.s
@@ -0,0 +1,77 @@
+/* Taken from the Bare Bones tutorial at osdev.org */
+/* Constants for Multiboot header */
+.set ALIGN,   1 << 0 /* Align loaded modules on page boundaries */
+.set MEMINFO, 1 << 1 /* Provide memory map */
+.set FLAGS,   ALIGN | MEMINFO /* The Multiboot 'flag' field */
+.set MAGIC,   0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS) /* That's kind of a cheezy checksum, if you ask me. */
+
+/* The Multiboot header that marks this program as a kernel */
+.section  .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+/*
+The multiboot standard does not define the value of the stack pointer register
+(esp) and it is up to the kernel to provide a stack. This allocates room for a
+small stack by creating a symbol at the bottom of it, then allocating 16384
+bytes for it, and finally creating a symbol at the top. The stack grows
+downwards on x86. The stack is in its own section so it can be marked nobits,
+which means the kernel file is smaller because it does not contain an
+uninitialized stack. The stack on x86 must be 16-byte aligned according to the
+System V ABI standard and de-facto extensions. The compiler will assume the
+stack is properly aligned and failure to align the stack will result in
+undefined behavior.
+*/
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384 # 16 KiB
+stack_top:
+
+/* The linker designates _start to be the entry point for the kernel.
+The bootloader jumps to this position when the kernel is loaded. */
+
+.section .text
+.global _start
+.type _start, @function
+_start:
+  /* At this point, the bootloader has put us in 32-bit protected mode
+     Interrupts and paging are disabled. This is all documented in the
+     Multiboot standard. The kernel has full control.
+
+    ###                     ######  ####### ####### #######
+     #       ##   #    #    #     # #     # #     #    #
+     #      #  #  ##  ##    #     # #     # #     #    #
+     #     #    # # ## #    ######  #     # #     #    #
+     #     ###### #    #    #   #   #     # #     #    #
+     #     #    # #    #    #    #  #     # #     #    #
+    ###    #    # #    #    #     # ####### #######    #
+  */
+
+  /* Set up the stack. Remember it grows downwards (i.e. esp is decremented 
+  when things are popped) */
+  mov $stack_top, %esp
+
+  /* TODO: Initialize the processor more, e.g. floats, load GDT, enable
+           paging, etc. */
+
+  /* Enter the high-level kernel. The ABI requires the stack is 16-byte
+  aligned at the time of the call instruction. So far, nothing has been
+  pushed, so 16-byte alignement is still intact. */
+
+  call kernel_main
+
+  /*Drop-off infinite loop: Disables interupts with cli, waits for the 
+  next interrupt with hlt, and then keep jumping back to hlt if an
+  interrupt comes through, like a non-maskable interrupt. */
+    cli
+1:  hlt
+    jmp 1b
+
+/* Set the size of the _start symbol to the current location '.' minus its
+start. This is useful for debugging, apparently. */
+
+.size _start, . - _start
